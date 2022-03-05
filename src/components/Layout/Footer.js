3561f23092c614863d1acb9a3271e13a53081ed7
Patrick Dunn <pmdunn78@gmail.com>
2022-03-04 23:33:30 -0500

    added fontawesome & styled footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Footer.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 const Footer = () => {
   return (
     <div>
       <div className="footer-layout">
-        <div className="footer-content">
-          <span>
+        <div className="footer-content pt-2">
+          <h5>
             &copy; Patrick Dunn 2022{" "}
             <a
               href="https://github.com/knightmac19/react-todos"
               rel="noopener noreferrer"
               target="_blank"
             >
-              <span>Github</span>
+              <FontAwesomeIcon icon={faGithub} className="fa-lg icon" />
             </a>
-          </span>
+          </h5>
         </div>
       </div>
     </div>
