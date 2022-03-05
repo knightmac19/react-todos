f8dad91e04ca30ab830a7e27cefd983d3f03b162
Patrick Dunn <pmdunn78@gmail.com>
2022-03-04 22:43:23 -0500

    initial go at footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./App.css";
 
+import Footer from './components/Layout/Footer';
+
 
 const App = () => {
   return (
     <div>
       <h2>Let's get started</h2>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer-layout">
+        Patrick Dunn 2022{" "}
+        <a
+          href="https://github.com/knightmac19/react-todos"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <span>Github</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
