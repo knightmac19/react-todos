109f09ad3185b05f3656b0a756a87766a2169224
Patrick Dunn <pmdunn78@gmail.com>
2022-03-04 23:05:04 -0500

    centered footer content

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -5,14 +5,18 @@ const Footer = () => {
   return (
     <div>
       <div className="footer-layout">
-        Patrick Dunn 2022{" "}
-        <a
-          href="https://github.com/knightmac19/react-todos"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <span>Github</span>
-        </a>
+        <div className="footer-content">
+          <span>
+            &copy; Patrick Dunn 2022{" "}
+            <a
+              href="https://github.com/knightmac19/react-todos"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <span>Github</span>
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   );
