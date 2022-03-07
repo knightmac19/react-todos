47b9817f1f7fa3634602aa48988998a295665ead
Patrick Dunn <pmdunn78@gmail.com>
2022-03-07 00:01:10 -0500

    restyling buttons with larger text

--- src/components/Content/AddNewBtn.js
+++ src/components/Content/AddNewBtn.js
@@ -4,11 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const AddNewBtn = (props) => {
+  // const []
+
   return (
     <Button style={{ backgroundColor: "#BF7515" }} className="mt-2 ">
-      <strong>
+      <h5>
         NEW <FontAwesomeIcon icon={faPlus} className="fa-lg " />
-      </strong>
+      </h5>
     </Button>
   );
 };
--- src/components/Content/ClearAllBtn.js
+++ src/components/Content/ClearAllBtn.js
@@ -5,7 +5,7 @@ const ClearAllBtn = (props) => {
   return (
     <Container className="d-flex flex-direction-row justify-content-end mb-2">
       <Row className="pt-0 pb-0 pl-0 p-2">
-        <Button color="danger">Clear All</Button>
+        <Button color="danger"><h5>Clear All</h5></Button>
       </Row>
     </Container>
   );
