55f436b1c2f71f754f63bdf7d62b859c70d6e7f0
Patrick Dunn <pmdunn78@gmail.com>
2022-03-08 19:24:30 -0500

    created TodoForm, autofocus on input

--- src/components/Content/NewModal.js
+++ src/components/Content/NewModal.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import TodoForm from "./TodoForm";
 
-function NewModal(props) {
+const NewModal = (props) => {
+  
+  
   return (
     <div>
       <Modal show={props.show} onHide={props.handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Add a Todo</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <TodoForm />
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={props.handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={props.handleClose}>
-            Save Changes
+            Add Todo
           </Button>
         </Modal.Footer>
       </Modal>
---
+++ src/components/Content/TodoForm.js
@@ -0,0 +1,25 @@
+import React, { useRef, useEffect } from "react";
+import { Form } from "react-bootstrap";
+
+const TodoForm = (props) => {
+  const focusInput = useRef();
+
+  useEffect(() => {
+    if (focusInput.current) focusInput.current.focus();
+  }, [focusInput]);
+
+  return (
+    <div>
+      <Form.Group>
+        <Form.Label>Todo Text</Form.Label>
+        <Form.Control
+          type="input"
+          ref={focusInput}
+          placeholder="Enter a todo..."
+        />
+      </Form.Group>
+    </div>
+  );
+};
+
+export default TodoForm;
