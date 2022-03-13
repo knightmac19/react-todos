92f155afbcd36a2c0888f50bb05f6c2f52021d75
Patrick Dunn <pmdunn78@gmail.com>
2022-03-13 10:56:52 -0400

    adding trash can icon to clear all btn

--- src/components/Content/ClearAllBtn.js
+++ src/components/Content/ClearAllBtn.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Container, Row, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const ClearAllBtn = (props) => {
   return (
     <Container className="d-flex flex-direction-row justify-content-end mb-2">
       <Row className="pt-0 pb-0 pl-0 p-2">
         <Button variant="danger" onClick={props.clearAllHandler}>
-          <h5>Clear All</h5>
+          <h5>
+            Clear All{" "}
+            <FontAwesomeIcon
+              className="clear-this-icon fa-lg"
+              aria-hidden="true"
+              icon={faTrashCan}
+            />
+          </h5>
         </Button>
       </Row>
     </Container>
