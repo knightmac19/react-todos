fb4b67c703f10d7f94e6bccac77a78e21a3bcd86
Patrick Dunn <pmdunn78@gmail.com>
2022-03-13 11:42:00 -0400

    fixing footer margin layout

--- src/components/Content/TodoTable.js
+++ src/components/Content/TodoTable.js
@@ -7,20 +7,18 @@ const TodoTable = (props) => {
   // console.log('in TodoTable')
   // console.log(props);
   return (
-    <Container className="">
+    <Container style={{ marginBottom: "80px" }} className="">
       <Row className="mb-2 justify-content-center">
         <Col className="col-lg-9 col-md-9 col-sm-12 col-12">
           <ul className="list-group">
             <li className="list-group-item bg-secondary text-light">
               <h3>Add Todos here...</h3>
-              
-              
             </li>
             <div className="list-header">
               {/* {dynamic li's go here} */}
-              
+
               {props.items.map((todo) => (
-                <TodoItem 
+                <TodoItem
                   key={todo.id}
                   id={todo.id}
                   text={todo.text}
