3548525c1f154bf9e5dd9fb62cc2dadb4546c545
Patrick Dunn <pmdunn78@gmail.com>
2022-06-26 15:08:59 -0400

    resize jumbotron, remove second ticker

--- src/components/Layout/Jumbotron.js
+++ src/components/Layout/Jumbotron.js
@@ -11,19 +11,19 @@ const Jumbotron = () => {
   };
 
   return (
-    <div className="mt-4 text-white rounded">
+    <div className="mt-2 text-white rounded">
       <Container className="">
-        <h1 className="pb-3">Todos Manager</h1>
+        {/* <h1 className="pb-3">Todos Manager</h1> */}
         <div className="text-center">
           <h4 className="datestamp">{dateTime.toString().substring(0, 15)}</h4>
-          <h5 className="timestamp">
+          {/* <h5 className="timestamp">
             {dateTime.toLocaleString("en-US", {
               hour: "numeric",
               minute: "numeric",
               second: "numeric",
               hour12: true,
             })}
-          </h5>
+          </h5> */}
           {updateTimestamp()}
           <hr />
         </div>
