580d98375b0526bffd82c9f1ad87e646461fff4d
Patrick Dunn <pmdunn78@gmail.com>
2022-06-26 15:07:04 -0400

    resizing clearAll btn

--- src/components/Content/ClearAllBtn.js
+++ src/components/Content/ClearAllBtn.js
@@ -6,16 +6,21 @@ import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 const ClearAllBtn = (props) => {
   return (
     <Container className="d-flex flex-direction-row justify-content-end mb-2">
-      <Row className="pt-0 pb-0 pl-0 p-2">
+      <Row 
+        className="pt-0 pb-0 pl-0"
+        style={{
+          paddingRight: '12px'
+        }}
+      >
         <Button variant="danger" onClick={props.clearAllHandler}>
-          <h5>
+          <h6>
             Clear All{" "}
             <FontAwesomeIcon
               className="clear-this-icon fa-lg"
               aria-hidden="true"
               icon={faTrashCan}
             />
-          </h5>
+          </h6>
         </Button>
       </Row>
     </Container>
