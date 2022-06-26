4442b2e66cce3f1e854e095ab02c92e1e10f3226
Patrick Dunn <pmdunn78@gmail.com>
2022-06-26 15:20:36 -0400

    created, styled new InputForm

---
+++ src/components/Content/InputForm.js
@@ -0,0 +1,46 @@
+import React, { useRef, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const InputForm = (props) => {
+  const focusInput = useRef();
+
+  useEffect(() => {
+    if (focusInput.current) focusInput.current.focus();
+  }, [focusInput]);
+
+  return (
+    <div>
+      <Form.Group
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "start",
+          margin: "5px",
+        }}
+      >
+        <Form.Control
+          type="input"
+          ref={focusInput}
+          placeholder="Enter a todo..."
+          onChange={props.todoChangeHandler}
+          onKeyPress={props.enterListener}
+        />
+        <Button
+          style={{
+            width: "25%",
+            // flexDirection:"row",
+            marginLeft: "15px",
+            fontSize: "0.9em",
+            fontWeight: "bolder",
+          }}
+        >
+           <FontAwesomeIcon icon={faPlus} className="fa-lg" />
+        </Button>
+      </Form.Group>
+    </div>
+  );
+};
+
+export default InputForm;
